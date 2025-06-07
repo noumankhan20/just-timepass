60c77bdd698a1bccdef4db31e9bff8c3840c25a5
noumankhan20 <noumankhan8097@gmail.com>
2025-06-07 13:07:10 +0530

    feat: configure environment variables and update app structure bohot galti ho rhi h

--- src/app.js
+++ src/app.js
@@ -1,1 +1,13 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import mongoose from "mongoose";
+
+const app=express()
+dotenv.config()
+
+app.use(express);
+
+
+
 console.log("abhi karunga  tabahi");
