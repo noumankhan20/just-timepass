589118b2447424d89b26d091d442fef66acafa1b
noumankhan20 <noumankhan8097@gmail.com>
2025-06-09 23:38:58 +0530

    added login functionality to user controller just add the function will do rest all tom

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -20,6 +20,24 @@ const registerUser= async (req, res) => {
     };
 
 
+const loginuser= async (req,res)=>{
+    const {email,password}=req.body;
+    
+   try {
+     const user = await User.findOne({
+         email:email,
+         password:password
+     })
+     if (!user){
+         return res.status(404).json({message:"Bhak laude tu user nhi h "})
+     }
+     res.status(200).json({message:"Login hogaye aap aaye andar aaye"});
+   } catch (error) {
+    res.status(500).json({message:"Internal server error",error:error.message});
+    
+   }
+}
 
-export {registerUser};
+
+export {registerUser,loginuser};
     
