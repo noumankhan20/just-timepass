b7906144407efd16b901c49e429f13a2f7d17b0d
noumankhan20 <noumankhan8097@gmail.com>
2025-06-11 14:54:04 +0530

    refactor database configuration to enable connection options

--- src/db/index.js
+++ src/db/index.js
@@ -6,11 +6,11 @@ dotenv.config();// read the .env file and make it available in process.env
 
 const dbconfig={
     url:process.env.MONGO_URL,
-    // options:{
-    //     useNewUrlParser: true,//these are the extra settings for the stable connections so they just 
-    //     //  ensures that this one useNewUrlParser is used to ensure correct url parsing 
-    //     useUnifiedTopology: true
-    // }
+    options:{
+        useNewUrlParser: true,//these are the extra settings for the stable connections so they just 
+        //  ensures that this one useNewUrlParser is used to ensure correct url parsing 
+        useUnifiedTopology: true
+    }
 }
 
 
